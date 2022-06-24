252d23ae3cabd8621e45e8d7b8f8c42670d3d3ce
yizhouzhao <yizhou_zhao@berkeley.edu>
2022-06-24 12:04:16 -0700

    change folder name

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/home'))
-  .get('/aboutme', (req, res) => res.render('pages/aboutme'))
-  .get('/dictionary', (req, res) => res.render('pages/dictionary'))
-  .get('/tactics2001', (req, res) => res.render('pages/tactics2001'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/home'))
+//   .get('/aboutme', (req, res) => res.render('pages/aboutme'))
+//   .get('/dictionary', (req, res) => res.render('pages/dictionary'))
+//   .get('/tactics2001', (req, res) => res.render('pages/tactics2001'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+const app = express();
+app.use('/', router);
+app.listen(PORT);
+
+console.log(`Running at Port ${PORT}`);
+
